1afff527489e2a597945ce0ea00366d23bca74ba
ParaskaBohdan <bogdannparaska@gmail.com>
2023-11-06 22:44:30 +0200

    test

--- src/pages/dwellings/Dwellings.jsx
+++ src/pages/dwellings/Dwellings.jsx
@@ -1,41 +1,39 @@
-import React from 'react';
-import {API_URL} from '../../index';
+import React, { useEffect, useState, useCallback } from 'react';
+import { API_URL } from '../../index';
 import ListDwellings from '../../components/listDwellings/ListDwellings';
 import axios from 'axios';
-import {useEffect, useState} from "react";
-import './style.css'
+import './style.css';
 
 const Dwellings = () => {
-    const [Dwellings, setDwellings] = useState([]);
+  const [Dwellings, setDwellings] = useState([]);
+  const URL = API_URL + '/api/dwellings';
+  console.log(URL);
+  const getDwellings = useCallback(() => {
+    axios.get(URL).then((data) => setDwellings(data.data));
+  }, [URL]);
 
-    useEffect(()=>{
-        getDwellings(setDwellings)
-    },[]);
+  useEffect(() => {
+    getDwellings();
+  }, [getDwellings]);
 
-    const URL = API_URL + '/api/dwellings/'
-    console.log(URL)
-    const getDwellings = (data)=>{
-        axios.get(URL).then(data => setDwellings(data.data))
-    }
 
-    const resetState = () => {
-        getDwellings();
-    };
-    return ( 
-        <div style={{ marginTop: '20px' }}>
+
+  const resetState = () => {
+    getDwellings();
+  };
+
+  return (
+    <div style={{ marginTop: '20px' }}>
       <div className="row">
         <div className="col">
-          {/* Перший блок */}
           <div className="list-dwellings">
             <ListDwellings Dwellings={Dwellings} resetState={resetState} newStudent={false} />
           </div>
         </div>
       </div>
-      <div className="row">
-        
-      </div>
+      <div className="row"></div>
     </div>
-    );
-}
- 
+  );
+};
+
 export default Dwellings;
