02c3624c1507c123a845429fbc8832001616f9b4
ParaskaBohdan <bogdannparaska@gmail.com>
2023-12-01 20:09:48 +0200

    fix

---
+++ src/components/searchBar/SearchBar.jsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+import DatePicker from '../datePicker/DatePicker';
+import DropCounter from '../dropCounter/dropCounter';
+import Search from '../search/Search';
+import './style.css';
+
+const SearchBar = ({onChange}) => {
+  const [searchValue, setSearchValue] = useState('');
+  const [adults, setAdults] = useState(1);
+  const [children, setChildren] = useState(0);
+  const [rooms, setRooms] = useState(1);
+  const [dates, setDates] = useState({ entryDate: '', exitDate: '' });
+  
+  const handleSearchChange = (value) => {
+    setSearchValue(value);
+  };
+  
+  const handleDateChange = (date) => {
+    setDates(date);
+  };
+
+  const handleCounterChange = (guests) => {
+    setAdults(guests.adults);
+    setChildren(guests.children);
+    setRooms(guests.rooms);
+    if (typeof onChange === 'function') {
+        onChange({ searchValue, dates, adults, children, rooms });
+      }
+  };
+
+//   useEffect(() => {}, [searchValue]);
+
+//   useEffect(() => {
+// }, [dates]);
+
+  useEffect(() => {
+  }, [adults, children, rooms]);
+  
+
+  return (
+    <div className="AllSearch">
+      <Search onChange={handleSearchChange} />
+      <DatePicker onChange={handleDateChange} />
+      <DropCounter onClick={handleCounterChange} />
+    </div>
+  );
+};
+
+export default SearchBar;
